6a2b07a1b534f38677f41dc8e3fc4d9246b4e8fe
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2022-09-14 14:23:29 +0300

    Допълнително упражнение

---
+++ Delegati 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Delegati
+{
+    class Program
+    {
+        public delegate List<int> returnEven(List<int> list);
+        public delegate List<int> returnOdd(List<int> list);
+        static void Main(string[] args)
+        {
+            returnEven even = (List<int> list) => list.Where(x => x % 2 == 0).ToList() ; ;
+            returnOdd odd = (List<int> list) => list.Where(x => x % 2 != 0).ToList(); ;
+
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Console.WriteLine("Even");
+            Console.WriteLine("...........................................");
+            Console.WriteLine(String.Join(" ", even(list)));
+            Console.WriteLine("Odd");
+            Console.WriteLine("...........................................");
+            Console.WriteLine(String.Join(" ",odd(list)));
+        }
+    }
+}
