4e9ea579c26b27674bfd60d0f1176beaddb5eafb
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2022-09-14 09:57:52 +0300

    Задача 1 14.09.2022

---
+++ Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {   public delegate void ReturnMessege(string msg);
+        static void Main(string[] args)
+        {
+
+            ReturnMessege returner = (string messegge) => Console.WriteLine("Dispatcher name changed to " + messegge); ;
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                returner(input);
+                input = Console.ReadLine(); 
+            }
+        }
+    }
+}
