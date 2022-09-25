121cfc4a120c42a2392b399e25f9f35b1475c9d8
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2022-09-25 10:03:54 +0300

    Задача решавана на 25.09

---
+++ uml/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Student student = new Student("Ivan Vazov 2", -19, "Plovdiv", "I", "000000000000", "Bio");
+            Emp emp = new Emp("Ivan Vazov 2", 19, "Plovdiv", "Ivan", "Tech", 50000.3);
+            Customer customer = new Customer("Ivan Vazov 2", 19, "Plovdiv", "Ivan", "1010", "Ivan Vazov 2");
+            
+            student.DoSomeWork();
+            emp.DoSomeWork();
+            customer.DoSomeWork();
+        }
+    }
+}
---
+++ uml/Student.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uml
+{
+    public class Student : Person, Interface1
+    {
+        private string rolleNumber;
+        private string studyingClass;
+
+        public string RolleNumber
+        {
+            get { return rolleNumber; }
+            set
+            {
+                try
+                {
+                     if (value.Length == 10)
+                     {
+                        rolleNumber = value;
+                     }
+                else
+                {
+                    Console.WriteLine("Roll Number must be 10 digits long");
+                    throw new Exception();
+                }
+                }
+                catch (Exception)
+                {
+                    string name = Console.ReadLine();
+                    while (name.Length < 2)
+                    {
+                        Console.WriteLine("Roll Number must be 10 digits long");
+                        name = Console.ReadLine();
+                    }
+                    this.rolleNumber = name; ;
+                    
+                }
+               
+            }
+        }
+
+        public string StudyingClass
+        {
+            get { return studyingClass; }
+            set
+            {
+                try
+                {
+                    if (value.Length < 2)
+                    {
+                        Console.WriteLine("Studying class must be atleast 2 letteres long");
+                        throw new Exception();
+                    }
+                    studyingClass = value;
+                }
+                catch (Exception)
+                {
+
+                    string name = Console.ReadLine();
+                    while (name.Length < 2)
+                    {
+                        Console.WriteLine("Studying class must be atleast 2 letteres long");
+                        name = Console.ReadLine();
+                    }
+                    this.studyingClass = name; ;
+                }
+                
+
+            }
+        }
+
+        public Student(string adress, int age, string city, string name, string rollNumber, string studyingClas)
+            : base(adress, age, city, name)
+        {
+            RolleNumber = rollNumber;
+            StudyingClass = studyingClas;
+        }
+
+        public void DoSomeWork()
+        {
+            Console.WriteLine($"Adress: {Adress}, Age: {Age}, City: {City}, Name: {Name}, Rolle number {RolleNumber},  Studying Class {StudyingClass}");
+        }
+    }
+}
