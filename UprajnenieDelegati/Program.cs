83d69658ed3988e0b8f18ec0c9d3b80060a07a09
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2022-09-26 15:42:54 +0300

    Малко упражнение 26.09

---
+++ UprajnenieDelegati/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Delegati
+{
+    class Program
+    {
+        public delegate int returnSum(int a, int b);
+        public delegate int returnDif(int a, int b);
+        public delegate List<double> solveQuadratic();
+        
+        static void Main(string[] args)
+        {
+            int i = 1;
+            returnSum returnSum = (int a, int b) => a + b;
+            returnDif returnDif = (int a, int b)=> a - b;
+
+            Console.WriteLine(returnSum(1, 7));
+            Console.WriteLine(returnDif(1, 7));
+            findQuadratic();
+             void findQuadratic()
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                List<double> result = new List<double>();
+                result.Add((-1 * b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a);
+                result.Add((-1 * b - Math.Sqrt(b * b - 4 * a * c)) / 2 * a);
+                foreach (var item in result)
+                {
+                    
+                    if (double.IsNaN(item))
+                    {
+                        Console.WriteLine("Something went wrong");
+                    }
+                    else Console.WriteLine($"X{i} is "+ item);
+                    i++;
+                }
+                
+            }
+        }
+       
+    }
+}
