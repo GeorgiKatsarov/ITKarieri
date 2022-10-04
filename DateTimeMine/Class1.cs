3da49abd054b393490702dfa330142d6cfaf2c45
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2022-10-04 23:24:46 +0300

    Задачка за валидация

---
+++ DateTimeMine/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeMine
+{
+    internal class Time
+    {
+        private int seconds;
+        private int minutes;
+        private int hours;
+        public int Seconds
+        {
+            get { return seconds; }
+            set
+            {
+                while (value > 60)
+                {
+                    Console.WriteLine("Seconds can't be bigger than 60, enter a new value");
+                    
+                    
+                    int value1 = int.Parse(Console.ReadLine());
+                    while (value1 > 60)
+                    {
+                        value1 = int.Parse(Console.ReadLine());
+                    }
+                    value = value1;
+                    break;
+                }
+                seconds = value;
+                
+
+            }
+        }
+        public int Minutes
+        {
+            get { return minutes; }
+            set
+            
+            {
+                while (value > 60)
+                {
+                    Console.WriteLine("Minutes can't be bigger than 60, enter a new value");
+                    int value1 = int.Parse(Console.ReadLine());
+                    while (value1 > 60)
+                    {
+                        value1 = int.Parse(Console.ReadLine());
+                    }
+                    value = value1;
+                    break;
+                }
+                minutes = value;
+                
+
+            }
+        }
+        public int Hours
+        {
+            get { return hours; }
+            set
+            {
+                while (value > 24)
+                {
+                    Console.WriteLine("Hours can't be bigger than 24, enter a new value");
+                    int value1 = int.Parse(Console.ReadLine());
+                    while (value1 > 24)
+                    {
+                        value1 = int.Parse(Console.ReadLine());
+                    }
+                    value = value1;
+                    break;
+                }
+                hours = value;
+                
+
+            }
+        }
+        public Time(int secunds, int minutes, int hours)
+        {
+            Seconds = secunds;
+            Minutes = minutes;
+            Hours = hours;
+        }
+        public void TellTime()
+        {
+            Console.WriteLine($"The time is {this.Hours}:{this.Minutes}:{this.Seconds}");
+        }
+    }
+}
---
+++ DateTimeMine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DateTimeMine
+{
+    public class DateTimeMine
+    {
+        public static void Main()
+        {
+            Time time = new Time(89, 42, 13);
+            Time time2 = new Time(59, 42, 26);
+            time.TellTime();
+            time2.TellTime();
+            
+
+        }
+    }
+}
